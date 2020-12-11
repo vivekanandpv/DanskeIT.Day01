9065932143628d49934cebc49659b66f599e8133
Vivekanand P V <vivekanandpv@outlook.com>
2020-12-11 08:50:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DanskeIT.Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
